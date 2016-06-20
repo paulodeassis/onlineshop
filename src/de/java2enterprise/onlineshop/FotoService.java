783271dbe52a1d1b7e98b63808e3582592e630d4
Paulo <paulodeassis.nascimento@gmail.com>
2016-06-20 19:26:00 -0300

    Atualização

--- src/de/java2enterprise/onlineshop/FotoService.java
+++ src/de/java2enterprise/onlineshop/FotoService.java
@@ -1,33 +1,55 @@
 package de.java2enterprise.onlineshop;
 
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 public class FotoService implements Runnable {
-private InputStream inputstream;
-private OutputStream outputstream;
+private AsyncContext async;
 
-public FotoService(InputStream is, OutputStream os){
-	this.inputstream=is;
-	this.outputstream=os;
+public FotoService(AsyncContext ac){
+	this.async=ac;
 }
 @Override
 public void run(){
+	final HttpServletRequest request = (HttpServletRequest)async.getRequest();
+	final HttpServletResponse response = (HttpServletResponse)async.getResponse();
+	
+	OutputStream outputstream = null;
+	InputStream inputstream = null;	
+	PrintWriter out = null;
+	
 	try{
-		byte[] buffer = new byte[1024];
-		int i=0;
-			while((i=inputstream.read(buffer))!=-1){
-				outputstream.write(buffer, 0, i);
-			}
-		}catch(Exception ex){
-			ex.printStackTrace();
-		}finally{
-			try{
-				outputstream.close();
-				inputstream.close();
-			}catch(Exception e){
-				e.printStackTrace();
-			}
+		final Part part = request.getPart("foto");
+		outputstream = new FileOutputStream(part.getSubmittedFileName());
+		inputstream = part.getInputStream();
+		
+		byte[] buffer  = new byte[1024];
+		int i = 0;
+		while((i = inputstream.read(buffer))!=-1){
+			outputstream.write(buffer, 0, i);
+		}
+		outputstream.flush();
+		out.write("true");
+		async.complete();
+		
+	}catch(Exception ex){
+		out.write("false");
+		ex.printStackTrace();
+	}finally{
+		try{
+			
+		}catch(Exception e){
+			e.printStackTrace();
 		}
 	}
 }
+
+}
+
