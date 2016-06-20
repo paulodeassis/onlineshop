a3a2f1a18480d59e0fb83d6620ce07cca6b9effc
Paulo <paulodeassis.nascimento@gmail.com>
2016-06-20 19:29:28 -0300

    Atualização para implementação de ajax na página.

--- src/de/java2enterprise/onlineshop/SellServlet.java
+++ src/de/java2enterprise/onlineshop/SellServlet.java
@@ -8,6 +8,7 @@ import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Properties;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -44,47 +45,8 @@ public class SellServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		final String title=request.getParameter("title");
-		final String description=request.getParameter("description");
-		final String price = request.getParameter("price");
-		
-		final Part part = request.getPart("foto");
-		
-		OutputStream outputstream = null;
-		InputStream inputstream = null;
-		
-		try{
-			String path = "C:\\tmp"+part.getSubmittedFileName();
-			File file = new File(path);
-			outputstream = new FileOutputStream(file);
-			byte[] buffer = new byte[1024];
-			int i=0;
-			inputstream = part.getInputStream();
-			while((i=inputstream.read(buffer))!= -1){
-				outputstream.write(buffer, 0, i);
-			}
-		}catch(Exception ex){
-			throw new ServletException(
-					ex.getMessage());
-		}finally{
-			outputstream.close();
-			inputstream.close();
-		}
-		
-		PrintWriter out = response.getWriter();
-		
-		
-		out.println("<!DOCTYPE html");
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<br>Hochgeladener Artikel: "+title);
-		out.println("<br>Beschreibung: "+description);
-		out.println("<br>Preis "+price);
-		out.println("<br>Bild "+part.getSubmittedFileName());
-		out.println("</body>");
-		out.println("</html>");
-		
+		final AsyncContext asyncron = request.startAsync();
+		asyncron.start(new FotoService(asyncron));		
 	}
 	
 	protected long PermitedFileSize() throws IOException{
